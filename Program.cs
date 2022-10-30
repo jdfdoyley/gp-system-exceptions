87b1908aa0a1adbbc81d96aaf28e512481feaee3
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-30 02:08:17 -0400

    Demo system generate exception

--- Program.cs
+++ Program.cs
@@ -11,8 +11,79 @@
 
 public class SystemExceptionDemo
 {
+    // Demonstrate throwing an exception when a division by zero error occurs
+    public static int Quotient(int numerator, int denominator)
+    {
+        return numerator / denominator;
+    }
+
     static void Main(string[] args)
     {
+        // Print a header line
+        Console.WriteLine(
+            "Jason D'Oyley - Week 4 GP - System Generated Exceptions"
+        );
 
+        bool cont = true; // loop control variable
+
+        do
+        {
+            try
+            {
+                Console.Write("Please enter an integer numerator: ");
+                string? val = Console.ReadLine();
+                int numerator = Convert.ToInt32(val);
+
+                Console.Write("Please enter an integer denominator: ");
+                val = Console.ReadLine();
+                int denominator = Convert.ToInt32(val);
+
+                int res = Quotient(numerator, denominator);
+                Console.WriteLine(
+                    "\nResult: {0} / {1} = {2}", numerator, denominator, res
+                );
+
+                cont = false;
+            }
+            catch (FormatException e)
+            {
+                // Print the exception that was caught
+                Console.WriteLine("\nException: {0}", e);
+
+                // Discard the input so the user can try again
+                Console.WriteLine(
+                    "You must enter integers. Please try again."
+                );
+            }
+            catch (DivideByZeroException e)
+            {
+                // Print the exception that was caught
+                Console.WriteLine("\nException: {0}", e);
+
+                // Discard the input so the user can try again
+                Console.WriteLine(
+                    "Zero is an invalid denominator. Please try again."
+                );
+            }
+
+        } while (cont);
+
+        // Now demonstrate what happens without exception handling
+        Console.WriteLine(
+            "Here's what would have happened with no Exception Handling:\n"
+        );
+        Console.Write("Please enter an integer numerator: ");
+        string? val2 = Console.ReadLine();
+        int numerator2 = Convert.ToInt32(val2);
+
+        Console.Write("Please enter an integer denominator: ");
+        val2 = Console.ReadLine();
+        int denominator2 = Convert.ToInt32(val2);
+        int res2 = Quotient(numerator2, denominator2);
+
+        // With invalid input, this line will not be reached
+        Console.WriteLine(
+            "\nResult: {0} / {1} = {2}", numerator2, denominator2, res2
+        );
     }
 }
