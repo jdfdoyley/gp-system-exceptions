fab5f090828b4ddce22ecbcda4a7d0b07d61aa13
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-30 01:08:18 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: October 30, 2022
+* Assignment CIS214 Week 4 GP - System Generated Exceptions
+*
+* Main application (program) class.
+* In this application we will demonstrate the use of exception handling to
+* properly process and handle a system-generated exception, allowing the program
+* to continue operating after an exception is thrown by the system.
+\******************************************************************************/
+
+public class SystemExceptionDemo
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# System Generated Exceptions
+
+In this application we will demonstrate the use of exception handling to
+properly process and handle a system-generated exception, allowing the program
+to continue operating after an exception is thrown by the system.
